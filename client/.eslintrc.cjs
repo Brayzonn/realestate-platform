68ea11784831389e28ce3d2aa94952d982d57c52
Brayzonn <brayzonn@gmail.com>
2025-06-19 16:38:24 +0100

    Add jsx-a11y plugin to ESLint configuration

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,15 +4,24 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',  
   },
+  settings: {
+    react: {
+      version: 'detect',
+      runtime: 'automatic'  
+    }
+  }
 }
