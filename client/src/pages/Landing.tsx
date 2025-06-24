6abcfee21296f0e9c9cdab35caa1db0a99b55e70
Brayzonn <brayzonn@gmail.com>
2025-06-24 23:05:42 +0100

    removed unused commented code

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,11 +1,8 @@
 import Hero from '@/components/layout/Hero';
 import FeaturedListings from '@/components/layout/FeaturedListings';
 import Footer from '@/components/layout/Footer';
-// import { useScrollRestoration } from '@/hooks/useScrollRestoration';
-
 
 const Landing = () => {
-  // useScrollRestoration(); 
   
   return (
     <div className="text-mainTextBlack min-h-screen">
