ec75ec3a16fcf9cc1324f29fdd3e2fe786c16f0d
Brayzonn <brayzonn@gmail.com>
2025-06-28 11:00:37 +0100

    fix: limited articles displayed to 3 in landing page

--- client/src/components/layout/landing/NewsUpdate.tsx
+++ client/src/components/layout/landing/NewsUpdate.tsx
@@ -14,7 +14,7 @@ const NewsUpdate = () => {
         </h2>
       </div>
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {blogPosts.map((article) => (
+        {blogPosts.slice(0, 3).map((article) => (
           <div key={article.id} className="group">
             <Link
               to={`/blog/${article.id}`}
